3570047267cfc9ea47144c2c27a8edf959a425bd
Zakharov Mikhail <zakharov@scanex.ru>
2016-08-17 18:44:52 +0300

    GWF layer

---
+++ dist/GMXPluginGFW/src/GmxGFWLayer.js
@@ -0,0 +1,41 @@
+/** GeoMixer virtual layer
+*/
+(function (){
+
+var defineClass = function() {
+    var GeoMixerGFWLayer = L.GFWLayerWithSlider.extend({
+        initFromDescription: function(layerDescription) {
+            this._gmxProperties = layerDescription.properties;
+            return this;
+        },
+        getGmxProperties: function() {
+            return this._gmxProperties;
+        }
+    });
+    L.gmx.addLayerClass('GFW', GeoMixerGFWLayer);
+    return GeoMixerGFWLayer;
+}
+
+
+if (window.gmxCore) {
+    gmxCore.addModule('GFWVirtualLayer', function() {
+        return {
+            layerClass: defineClass()
+        }
+    }, {
+        init: function(module, path) {
+            if (!L.GFWLayerWithSlider) {
+                gmxCore.loadCSS(path + 'L.GFWSlider.css');
+                return $.when(
+                    gmxCore.loadScript(path + 'L.GFWLayer.js'),
+                    gmxCore.loadScript(path + 'L.GFWSlider.js')
+                );
+            }
+        }
+        
+    });
+} else {
+    defineClass();
+}
+ 
+})();
