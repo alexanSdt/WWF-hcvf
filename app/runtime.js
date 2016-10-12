c4070eb1957cfd22b86a8287336bb12b4127db1c
zverev <alexander.zverev@gmail.com>
2016-10-13 01:47:09 +0300

    add a hack for MLT group description

--- app/runtime.js
+++ app/runtime.js
@@ -1,10 +1,44 @@
 require('./magnific-popup/dist/jquery.magnific-popup.js')
 require('./magnific-popup/dist/magnific-popup.css')
 
+const $ = require('jquery')
 const SearchControl = require('./SearchControl')
 const HeaderContainerControl = require('./HeaderContainerControl')
 
+// безопасно разруливаем получение свойств глубоких объектов
+// вместо obj.foo.bar.baz пишем ensureProperty(obj, 'foo.bar.baz')
+function ensureProperty(o, p) {
+    let arP = p.split('.')
+    while (arP.length) {
+        o = o[arP.shift()]
+        if (!o) {
+            return false
+        }
+    }
+    return o
+}
+
 module.exports = function(cm) {
+    cm.define('mltGroupDescription', ['layersTree'], function (cm) {
+        const layersTree = cm.get('layersTree')
+        const mltGroupNode = layersTree.find('GxMKgtMviEPv92yl')
+        const descriptionNode = layersTree.find('09FD3B0FE2F94916B73119848046F408')
+        const description = ensureProperty(descriptionNode.attributes, 'properties.MetaProperties.desc_long.Value')
+        if (!mltGroupNode || !description ) {
+            return false
+        }
+
+        mltGroupNode.set('properties', $.extend(true, {}, mltGroupNode.get('properties'), {
+            MetaProperties: {
+                'desc_long': {
+                    Value: description
+                }
+            }
+        }))
+
+        return null
+    })
+
     cm.define('layersTreeWidgetDescriptionPopup', ['layersTreeWidget'], function (cm) {
         return cm.get('layersTreeWidget').on('infoButtonClick', (infoProperty, model) => (
             $.magnificPopup.open({
