095ec161b5e985144e6ba1e8cb8273181cd29cd9
zverev <alexander.zverev@gmail.com>
2016-10-13 15:21:55 +0300

    add winnie-core submodule

--- app/entry.js
+++ app/entry.js
@@ -6,6 +6,11 @@ window.Promise = window.Promise || require('es6-promise')
 require('../external/IconSidebarControl/dist/iconSidebarControl.js')
 require('../external/IconSidebarControl/dist/iconSidebarControl.css')
 
+// старый winnie-core не поддерживает сайдбар новой версии
+// берём из external ветку v2
+require('../external/winnie-core/dist/gmxApplication.js')
+require('../external/winnie-core/dist/gmxApplication.css')
+
 $(document).ready(function (je) {
     $.ajax('./app/config.json').then(function (resp) {
         var cm = window.cm = nsGmx.createGmxApplication($('body')[0], resp);
