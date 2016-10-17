746af3082b5324a53ba9252ba17afd44cb554db6
zverev <alexander.zverev@gmail.com>
2016-10-17 16:37:11 +0300

    move config.json to /resources

--- app/entry.js
+++ app/entry.js
@@ -14,7 +14,7 @@ require('../external/winnie-core/dist/gmxApplication.js')
 require('../external/winnie-core/dist/gmxApplication.css')
 
 $(document).ready(function (je) {
-    $.ajax('./app/config.json').then(function (resp) {
+    $.ajax('./resources/config.json').then(function (resp) {
         var cm = window.cm = nsGmx.createGmxApplication($('body')[0], resp);
 
         require('./runtime.js')(cm)
