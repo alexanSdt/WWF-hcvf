22232c6c3e5253e03adc1fb9e22ecf2700a2f677
zverev <alexander.zverev@gmail.com>
2016-10-17 16:44:48 +0300

    move marker icons to /resources

--- app/entry.js
+++ app/entry.js
@@ -13,9 +13,24 @@ require('../external/IconSidebarControl/dist/iconSidebarControl.css')
 require('../external/winnie-core/dist/gmxApplication.js')
 require('../external/winnie-core/dist/gmxApplication.css')
 
+// так как leaflet попадает в сборку, для него меняются пути к маркерам
+// устанавливаем новые:
+L.Icon.Default = L.Icon.Default.extend({
+    options: {
+        iconUrl: 'resources/marker-icon.png',
+        shadowUrl: 'resources/marker-shadow.png'
+    }
+})
+L.Icon.Default.imagePath = 'resources'
+L.Marker = L.Marker.extend({
+    options: {
+        icon: new L.Icon.Default()
+    }
+})
+
 $(document).ready(function (je) {
     $.ajax('./resources/config.json').then(function (resp) {
-        var cm = window.cm = nsGmx.createGmxApplication($('body')[0], resp);
+        var cm = window.cm = nsGmx.createGmxApplication($('body')[0], resp)
 
         require('./runtime.js')(cm)
 
